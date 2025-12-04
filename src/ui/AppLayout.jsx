65a323a1ed6864693b1aa1267c91389b9f5e9f8d
long.pham <long.pham@kohyoung.com>
2025-12-04 18:45:46 +0700

    New commit

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,10 +1,18 @@
 import { Outlet } from "react-router";
 import Header from "./Header";
 import Footer from "./Footer";
+import { useNavigation } from "react-router";
+import Loader from "./Loader";
+import styles from "./AppLayout.module.css";
 
 function AppLayout() {
+  const navigation = useNavigation();
+
+  const isLoading = navigation.state === "idle";
+
   return (
-    <div>
+    <div className={styles.appLayout}>
+      {isLoading && <Loader />}
       <Header />
       <Outlet />
       <Footer />
